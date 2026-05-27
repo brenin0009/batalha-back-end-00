6a4e7af2da63ce580ba4b77a338b778c04949529
brenin0009 <breno.escudeiro.santos@escola.pr.gov.br>
2026-05-27 11:03:56 -0300

    Add files via upload

---
+++ nn.js
@@ -0,0 +1,11 @@
+
+function lutar(){
+const classe = document.getElementById('classe').value;
+const arma = document.getElementById('arma').value.trim() ;
+const log = document.getElementById('log');
+
+if(!arma){
+log.innerHTML= "Você esqueceu de equipar uma arma"
+}
+let dado = Math.floor(Math.random() * 20) + 1;
+}
